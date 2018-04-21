0c06873f2241c9db2489ddda2e4de0d0783b4621
csanjeev25 <sanjeevkumarchintakindi@gmail.com>
2018-04-21 06:50:42 +0530

    Added view and test query

--- GameofThrones/urls.py
+++ GameofThrones/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.conf.urls import url
 from GameofThronesApp import views
 
+
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
--- GameofThronesApp/views.py
+++ GameofThronesApp/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 import cx_Oracle
 from django.http import HttpResponse
+from .models import House
 
 # Create your views here.
 
 def index(request):
-	con = cx_Oracle.connect('system/oracle@127.0.0.1:1521/xe')
+	'''con = cx_Oracle.connect('system/oracle@127.0.0.1:1521/xe')
 	cur = con.cursor()
 	cur.execute('SELECT * from battle')
 	result = cur.fetchall()
-	return HttpResponse("Valar Morghulis")
+	return HttpResponse("Valar Morghulis")'''
+	houses = House.objects.all()
+	return render(request,'index.html',{'houses':houses})
