b94e349a40b860a48d2130203785932eac7033c3
csanjeev25 <sanjeevkumarchintakindi@gmail.com>
2018-04-21 06:58:27 +0530

    Added admin panel

--- GameofThronesApp/admin.py
+++ GameofThronesApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Character
+from .models import House
+from .models import Battle
 
 # Register your models here.
+admin.site.register(Character)
+admin.site.register(House)
+admin.site.register(Battle)
